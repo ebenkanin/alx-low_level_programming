671f7d63b5f632115fe8fa897cf21b866bf3ac71
ebenkanin <ebenezerkanin@gmail.com>
2023-02-16 07:28:27 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints with puts function
+ * Return: 0 (this output will indicate accuracy)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
