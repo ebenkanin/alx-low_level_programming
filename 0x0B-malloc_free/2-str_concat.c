a5870146929a4e5f69d08f112db1a8dcb82e8713
ebenkanin <ebenezerkanin@gmail.com>
2023-03-15 05:12:34 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,17 +12,19 @@ char *str_concat(char *s1, char *s2)
 	int i, ci;
 
 	if (s1 == NULL)
-		s1 = "";
+		S1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	i = ci = 0;
 	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
 		return (NULL);
-
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
@@ -35,8 +37,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
