71f85d970d1b9e634bf83eadba22fe57fa1e3334
ebenkanin <ebenezerkanin@gmail.com>
2023-03-15 05:18:48 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,14 +5,14 @@
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat of s1 and s2
- **/
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
-		S1 = "";
+		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
@@ -40,3 +40,4 @@ char *str_concat(char *s1, char *s2)
 	conct[i] = '\0';
 	return (conct);
 }
+
